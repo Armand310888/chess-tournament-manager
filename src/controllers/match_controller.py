424c14b67c688cc630c4039835f4b618a389ebc7
Armand310888 <armand.delaporte@gmail.com>
2026-04-16 10:18:11 +0200

    feat: add Match controller
    - create match_controller module
    - prepare structure for match creation and management logic

---
+++ src/controllers/match_controller.py
@@ -0,0 +1,17 @@
+from models.match import Match
+
+def create_match(white_player: Player, black_player: Player) -> Match:
+    matches = load_matches() #depuis JSON
+    next_id = compute_next_id(matches)
+
+    return Match(
+        white_player=white_player,
+        black_player=black_player,
+        match_id=next_id
+    )
+
+def load_matches():
+    pass
+
+def compute_next_id():
+    pass
