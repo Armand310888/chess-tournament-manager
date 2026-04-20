558acdfb93d3248aa22803a7e6c77a8fcbc00a1a
Armand310888 <armand.delaporte@gmail.com>
2026-04-20 15:47:40 +0200

    feat: add initial Tournament model
    - define Tournament class with core attributes
    - include rounds and players relationships
    - initialize tournament metadata (name, place, description)
    - add round tracking with actual_round and number_of_rounds

--- src/models/tournament.py
+++ src/models/tournament.py
@@ -1,3 +1,23 @@
+from models.round import Round
+from models.player import Player
 
 
+class Tournament:
+    def __init__(
+            self,
+            name: str,
+            place: str,
+            list_of_rounds: list[Round],
+            list_of_players: list[Player],
+            number_of_rounds: int = 4,
+            actual_round: int = 1,
+            description: str = None
+            ):
 
+        self.name = name
+        self.place = place,
+        self.number_of_rounds = number_of_rounds
+        self.actual_round = actual_round
+        self.list_of_rounds = list_of_rounds
+        self.list_of_players = list_of_players
+        self.description = description
