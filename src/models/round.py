13cc3fde56f11460f57ef99e7884b86051cb9d79
Armand310888 <armand.delaporte@gmail.com>
2026-04-20 11:00:43 +0200

    feat (models): implement initial Round model

---
+++ src/models/round.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from enum import Enum
+
+from models.match import Match
+from models.lifecyle import start_lifecycle, end_lifecycle, EventStatus
+
+
+class Round:
+    def __init__(
+            self,
+            list_of_match: list[Match]
+
+    ):
+
+        self.list_of_match = list_of_match
+        self.start_datetime: datetime | None = None
+        self.end_datetime: datetime | None = None
+        self.round_number: int | None = None
+        self.status = EventStatus.NOT_STARTED
+
+    def start_round(self):
+        start_lifecycle(self)
+
+    def end_round(self):
+        end_lifecycle(self)
