f98a10bb9f9e2da9e83b8f805ffb2fc9b39e0bf0
Armand310888 <armand.delaporte@gmail.com>
2026-04-22 15:44:31 +0200

    feat(view): add validation loop for chess national ID input
    Integrate validate_regex_match in PlayerView to enforce correct chess national ID format at input stage.
    Introduce loop to repeatedly prompt user until a valid ID is provided.
    Standardize input prompts formatting and prepare view for further input validation integration.

--- src/views/player_view.py
+++ src/views/player_view.py
@@ -1,22 +1,42 @@
+import re
 
+from utils.validators import (
+    validate_regex_match,
+    CHESS_NATIONAL_ID_PATTERN,
+    CHESS_NATIONAL_ID_PATTERN_DESCRIPTION
+)
 
+# prévoir la validation des inputs à l'aide de mes validators
 class PlayerView:
     def prompt_for_player(self):
         first_name = input(
-            "Entrez le prénom du joueur : "
+            "Entrez le prénom du joueur: "
             )
         last_name = input(
-            "Entrez le nom de famille du joueur : "
+            "Entrez le nom de famille du joueur: "
             )
         birth_date = input(
-            "Entrez la date de naissance du joueur : "
+            "Entrez la date de naissance du joueur: "
             )
         elo_rating = input(
             "Entrez le classement ELO du joueur: "
             )
-        chess_national_id = input(
-            "Entrez l'identifiant national d'échecs du joueur: "
+        while True:
+            raw_chess_national_id = input(
+                "Enter player chess national ID: "
             )
+
+            try:
+                chess_national_id = validate_regex_match(
+                    raw_chess_national_id,
+                    "chess_national_id",
+                    CHESS_NATIONAL_ID_PATTERN,
+                    CHESS_NATIONAL_ID_PATTERN_DESCRIPTION
+                )
+                break
+
+            except ValueError as error:
+                print(error)
 
         return {
             "first_name": first_name,
