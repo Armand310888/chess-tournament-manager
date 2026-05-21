3b1ec218c6a13be6cf84add5088e94c5163ee66f
Armand310888 <armand.delaporte@gmail.com>
2026-05-21 22:50:25 +0200

    refactor: remove unused player selection method
    - Delete unused `select_players` method from PlayerController
    - Keep player creation flow unchanged
    - Clean up dead controller code

--- src/controllers/player_controller.py
+++ src/controllers/player_controller.py
@@ -25,7 +25,3 @@ class PlayerController:
         self.players.append(player)
         save_players(self.players)
         return player
-    
-    def select_players(self, players: list[Player]) -> list[Player]:
-        """"""
-        
